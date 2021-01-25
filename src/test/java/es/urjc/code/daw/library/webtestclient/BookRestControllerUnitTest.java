892293c7c06b120f71fc734182dc2b69cdb001fc
Miguel G. Sanguino <sanguino@me.com>
2021-01-25 21:41:44 +0100

    code style

--- src/test/java/es/urjc/code/daw/library/webtestclient/BookRestControllerUnitTest.java
+++ src/test/java/es/urjc/code/daw/library/webtestclient/BookRestControllerUnitTest.java
@@ -5,14 +5,18 @@ import es.urjc.code.daw.library.book.Book;
 import es.urjc.code.daw.library.book.BookService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
@@ -20,6 +24,7 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
@@ -71,16 +76,21 @@ public class BookRestControllerUnitTest {
     @Test
     @WithMockUser(username = "user", password = "pass", roles = "USER")
     public void addBookTest() {
-        Book book = new Book("book1", "desc1");
-        when(bookService.save(book)).thenReturn(book);
+        Book book = new Book("book1", "description1");
+        when(bookService.save(any(Book.class))).thenReturn(book);
 
+        String bookToCreate = "{\"title\":\"book1\",\"description\":\"description1\" }";
         this.webTestClient.
             post()
             .uri("/api/books/")
-            .body(Mono.just(book), Book.class)
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .body(BodyInserters.fromValue(bookToCreate))
             .exchange()
             .expectStatus()
-            .isCreated();
+            .isCreated()
+            .expectBody()
+            .jsonPath("$.title").isEqualTo("book1")
+            .jsonPath("$.description").isEqualTo("description1");
     }
 
     @Test
